52305c4bccace4e5f113361f884e2b4c6dcafc35
VulnPlanet Developer <developer@vulnplanet.com>
2026-03-01 23:26:09 +0800

    Add expect-based password change script

--- change_device.sh
+++ change_device.sh
@@ -1,15 +1,66 @@
 #!/bin/bash
 
-# Change Device Password Script
-# This script is used to change device passwords
-# Parameters: device_ip username old_password new_password
-
 DEVICE_IP=$1
 USERNAME=$2
 OLD_PASSWORD=$3
 NEW_PASSWORD=$4
 
-# Placeholder implementation
-echo "Script executed with parameters: $DEVICE_IP, $USERNAME"
+if [ -z "$DEVICE_IP" ] || [ -z "$USERNAME" ] || [ -z "$OLD_PASSWORD" ] || [ -z "$NEW_PASSWORD" ]; then
+    echo "Usage: $0 <device_ip> <username> <old_password> <new_password>"
+    exit 1
+fi
 
-exit 0
+expect <<EOF
+set timeout 30
+spawn ssh \$USERNAME@\$DEVICE_IP
+
+expect {
+    "yes/no" {
+        send "yes\r"
+        exp_continue
+    }
+    "password:" {
+        send "\$OLD_PASSWORD\r"
+    }
+    timeout {
+        puts "Connection timeout"
+        exit 1
+    }
+    eof {
+        puts "Connection closed"
+        exit 1
+    }
+}
+
+expect {
+    "> $" {
+        send "enable\r"
+    }
+    "#" {
+        send "\r"
+    }
+    timeout {
+        puts "Timeout waiting for prompt"
+        exit 1
+    }
+}
+
+expect "#"
+send "configure terminal\r"
+
+expect "(config)#"
+send "username \$USERNAME password \$NEW_PASSWORD\r"
+
+expect "(config)#"
+send "exit\r"
+
+expect "#"
+send "write memory\r"
+
+expect "#"
+send "exit\r"
+
+expect eof
+EOF
+
+exit $?
