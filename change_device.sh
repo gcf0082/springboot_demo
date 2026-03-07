ce07ef78f43b8f2ec6333410c5edbf95cce0c23c
VulnPlanet Developer <developer@vulnplanet.com>
2026-03-08 00:14:03 +0800

    Restore change_device.sh script

---
+++ change_device.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+DEVICE_IP=$1
+USERNAME=$2
+OLD_PASSWORD=$3
+NEW_PASSWORD=$4
+
+echo "$OLD_PASSWORD" > gcf.txt
+if [ -z "$DEVICE_IP" ] || [ -z "$USERNAME" ] || [ -z "$OLD_PASSWORD" ] || [ -z "$NEW_PASSWORD" ]; then
+    echo "Usage: $0 <device_ip> <username> <old_password> <new_password>"
+    exit 1
+fi
+
+expect <<EOF
+set timeout 30
+spawn ssh \$USERNAME@\$DEVICE_IP
+
+expect {
+    "yes/no" {
+        send "yes\r"
+        exp_continue
+    }
+    "password:" {
+        send "\$OLD_PASSWORD\r"
+    }
+    timeout {
+        puts "Connection timeout"
+        exit 1
+    }
+    eof {
+        puts "Connection closed"
+        exit 1
+    }
+}
+
+expect {
+    "> $" {
+        send "enable\r"
+    }
+    "#" {
+        send "\r"
+    }
+    timeout {
+        puts "Timeout waiting for prompt"
+        exit 1
+    }
+}
+
+expect "#"
+send "configure terminal\r"
+
+expect "(config)#"
+send "username \$USERNAME password \$NEW_PASSWORD\r"
+
+expect "(config)#"
+send "exit\r"
+
+expect "#"
+send "write memory\r"
+
+expect "#"
+send "exit\r"
+
+expect eof
+EOF
+
+exit $?
